6f9761f04d172047489152eeb351052a3f49f686
git-himansh01 <waytohimanshu@gmail.com>
2024-03-10 03:36:56 +0530

    Added custom Exception handler

---
+++ src/main/java/com/learning/mobileappws/exception/AppExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.learning.mobileappws.exception;
+
+import com.learning.mobileappws.ui.model.response.ErrorMessage;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value={Exception.class})
+    public ResponseEntity<Object> handleAnyException(Exception exp, WebRequest request){
+        String errorMessageDescription = exp.getLocalizedMessage();
+        //   if(errorMessageDescription == null) errorMessageDescription= exp.getMessage();
+        // ErrorMessage errorMessage= new ErrorMessage(new Date(), exp.getLocalizedMessage());
+        if(errorMessageDescription == null) errorMessageDescription = exp.toString();
+        ErrorMessage errorMessage= new ErrorMessage(new Date(), errorMessageDescription);
+     //   return new ResponseEntity<>(exp, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
---
+++ src/main/java/com/learning/mobileappws/ui/model/response/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.learning.mobileappws.ui.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private Date timestamp;
+    private String message;
+}
