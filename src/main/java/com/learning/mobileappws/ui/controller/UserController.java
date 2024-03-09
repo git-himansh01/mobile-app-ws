2ae07fbf99822c644607fe3799c8e571d4caf969
git-himansh01 <waytohimanshu@gmail.com>
2024-03-09 23:30:23 +0530

    Created a Controller with Request Mappings and Response Entity

--- src/main/java/com/learning/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/learning/mobileappws/ui/controller/UserController.java
@@ -1,14 +1,41 @@
 package com.learning.mobileappws.ui.controller;
 
+import com.learning.mobileappws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
-    @GetMapping("/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "Get User was called "+userId;
+//required = false making the request param optional
+    @GetMapping
+    public String getUsers(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="25") int limit, @RequestParam(value="sort", defaultValue="desc", required=false ) String sort){
+        return "Get Users was called with page = ."+ page +" and limit ="+limit+" and sort = "+sort;
     }
+    //since var name in path and argument is same hence mentioning it in path variable argument is optional
+//    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+//    public UserRest getUser(@PathVariable(value="userId") String userId){
+//        UserRest userRest= new UserRest();
+//        userRest.setFirstName("Himanshu");
+//        userRest.setLastName("Gupta");
+//        userRest.setEmail("waystohimanshu@gmail.com");
+//        userRest.setUserId(userId);
+//        return userRest;
+//    }
+
+@GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+public ResponseEntity<UserRest> getUser(@PathVariable(value="userId") String userId) {
+    UserRest userRest = new UserRest();
+    userRest.setFirstName("Himanshu");
+    userRest.setLastName("Gupta");
+    userRest.setEmail("waystohimanshu@gmail.com");
+    userRest.setUserId(userId);
+    return new ResponseEntity<UserRest>(userRest, HttpStatus.OK);
+}
     @PostMapping
     public String createUser(){
         return "Create User was called";
