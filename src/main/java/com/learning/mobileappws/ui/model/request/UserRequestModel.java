4690c8128d4c72d78ab421993f00c8343a33f56f
git-himansh01 <waytohimanshu@gmail.com>
2024-03-09 23:38:12 +0530

    Created a User Request Model Class

---
+++ src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
@@ -0,0 +1,11 @@
+package com.learning.mobileappws.ui.model.request;
+
+import lombok.Data;
+
+@Data
+public class UserRequestModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userId;
+}
